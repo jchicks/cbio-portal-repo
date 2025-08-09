47ab90f1187de39b14b893ee300d1ea5a9e386c6
jchicks <githicks@gmail.com>
2025-08-09 16:36:15 -0400

    testing

--- src/main/java/org/cbioportal/application/security/config/NoSecurityConfig.java
+++ src/main/java/org/cbioportal/application/security/config/NoSecurityConfig.java
@@ -3,22 +3,45 @@ package org.cbioportal.application.security.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @ConditionalOnProperty(value = "authenticate", havingValue = "false", matchIfMissing = true)
 public class NoSecurityConfig {
+  //  @Bean
+  //  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  //    return http.cors(Customizer.withDefaults())
+  //        .csrf(AbstractHttpConfigurer::disable)
+  //        .authorizeHttpRequests(
+  //            auth -> auth.requestMatchers("/").permitAll().anyRequest().permitAll())
+  //        .build();
+  //  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http.cors(Customizer.withDefaults())
-        .csrf(AbstractHttpConfigurer::disable)
+    return http.cors(
+            cors ->
+                cors.configurationSource(
+                    request -> {
+                      var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                      corsConfig.addAllowedOrigin(
+                          "http://localhost:3000"); // Allow React app origin
+                      corsConfig.addAllowedMethod("*"); // Allow all HTTP methods
+                      corsConfig.addAllowedHeader("*"); // Allow all headers
+                      corsConfig.setAllowCredentials(
+                          true); // Allow sending credentials like cookies
+                      return corsConfig;
+                    }))
+        .csrf(csrf -> csrf.disable()) // Disable CSRF protection
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers("/").permitAll().anyRequest().permitAll())
+            auth ->
+                auth.requestMatchers("/")
+                    .permitAll()
+                    .anyRequest()
+                    .permitAll()) // Allow all requests
         .build();
   }
 }
